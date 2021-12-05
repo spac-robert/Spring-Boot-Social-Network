6b22a85237e7c78c2f06e2d761225c8310f5293f
spac-robert <spac-robert@github.com>
2021-12-06 01:25:18 +0200

    Added Lohin.html

--- src/main/java/ro/robert/socialmediaspringboot/controller/RegisterController.java
+++ src/main/java/ro/robert/socialmediaspringboot/controller/RegisterController.java
@@ -1,4 +1,15 @@
 package ro.robert.socialmediaspringboot.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/register")
 public class RegisterController {
+
+    @GetMapping
+    public String signUp() {
+        return "Register";
+    }
 }
