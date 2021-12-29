b10c86c43ccd09491c881a8af99ba11ba55d5b78
spac-robert <spacrobert.florian@gmail.com>
2021-12-29 18:57:10 +0200

    Add friend

--- src/main/java/ro/robert/socialmediaspringboot/repository/UserFriendsRepository.java
+++
@@ -1,9 +0,0 @@
-package ro.robert.socialmediaspringboot.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ro.robert.socialmediaspringboot.entity.User;
-
-@Repository
-public interface UserFriendsRepository extends JpaRepository<User, Long> {
-}
